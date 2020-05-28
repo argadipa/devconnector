5222efc7724fd00a45c30c9f3e7781ba2b02c88d
argadipame <argadipa.pw@mecreativeid.com>
2020-05-28 13:10:41 +0700

    Add route for delete profile and user.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,5 +143,34 @@ router.get(
 	}
 );
 
+// @route	DELETE api/profile
+// @desc 	Delete profile, user & posts
+// @access 	Private
+router.delete(
+	'/',
+	auth,
+	async (req, res) => {
+		try{
+			//@TODO -remove user posts
+
+			// Remove profile
+			await Profile.findOneAndRemove({ user: req.user.id });
+
+			await User.findOneAndRemove({ _id: req.user.id });
+
+			res.json({ msg: 'User deleted.'});
+
+		}catch(err){
+			console.error(err.message);
+
+			if(err.kind == 'ObjectId'){
+				return res.status(400).json({ msg: 'Profile not found.' });
+			}
+
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
 
 module.exports = router;
